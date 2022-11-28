2ba2120df49ae74e91818b82c5e8383b5bbe93b5
johnamcconnell <john.a.mcconnell@gmail.com>
2022-11-27 22:11:18 -0600

    second push with updated metadata

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,47 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Ask Marcus Aurelius Anything" key="title"/>
+        <meta property="og:description" content="built by John 🚢 together with buildspace" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:title" content="Tea time with Marcus Aurelius">
+        <meta name="twitter:description" content="Ever wonder what it would be like to sit down and chat with Marcus Aurelius? Now you can with the power of GPT-3">
+        <meta itemprop="image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png">
+
+        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+
+        <link rel="icon" type="image/x-icon" href="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/marcus-aurelius-gettyimages-122316830.ZA9Ai6KCZ02q28B9AAVp0A.jpg">
+
+
+        <!-- Google / Search Engine Tags -->
+
+        <!-- Twitter Meta Tags -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png">
+        <meta property="twitter:url" content="https://marcus.newagestoic.com">
+
+        <meta name="keywords" content="build, react, node, buildspace, gpt-3, ai, ml">
+        <meta name="robots" content="index, follow">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="language" content="English">
+
+        <!-- Primary Meta Tags -->
+        <title>GTFOL</title>
+        <meta name="title" content="Tea time with Marcus Aurelius">
+        <meta name="description" content="Ever wonder what it would be like to sit down and chat with Marcus Aurelius? Now you can with the power of GPT-3">
+
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://marcus.newagestoic.com">
+        <meta property="og:title" content="GTFOL">
+        <meta property="og:description" content="Ever wonder what it would be like to sit down and chat with Marcus Aurelius? Now you can with the power of GPT-3">
+<meta property="og:image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png">
+
+
       </Head>
       <body>
         <Main />
