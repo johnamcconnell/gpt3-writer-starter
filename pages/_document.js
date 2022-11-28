9777d4f72690e3d8d17605f49f102466f03bd8a2
johnamcconnell <john.a.mcconnell@gmail.com>
2022-11-27 22:20:02 -0600

    fourth push fixed closing meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -11,38 +11,38 @@ export default function Document() {
           content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
-        <meta name="twitter:title" content="Tea time with Marcus Aurelius">
-        <meta name="twitter:description" content="Ever wonder what it would be like to sit down and chat with Marcus Aurelius? Now you can with the power of GPT-3">
-        <meta itemprop="image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png">
+        <meta name="twitter:title" content="Tea time with Marcus Aurelius"></meta>
+        <meta name="twitter:description" content="Ever wonder what it would be like to sit down and chat with Marcus Aurelius? Now you can with the power of GPT-3"></meta>
+        <meta itemprop="image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png"></meta>
 
-        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <meta content="width=device-width, initial-scale=1" name="viewport"></meta>
 
-        <link rel="icon" type="image/x-icon" href="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/marcus-aurelius-gettyimages-122316830.ZA9Ai6KCZ02q28B9AAVp0A.jpg">
+        <link rel="icon" type="image/x-icon" href="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/marcus-aurelius-gettyimages-122316830.ZA9Ai6KCZ02q28B9AAVp0A.jpg"></link>
 
 
-        <!-- Google / Search Engine Tags -->
+        //Google / Search Engine Tags
 
-        <!-- Twitter Meta Tags -->
-        <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png">
-        <meta property="twitter:url" content="https://marcus.newagestoic.com">
+        // Twitter Meta Tags
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png"></meta>
+        <meta property="twitter:url" content="https://marcus.newagestoic.com"></meta>
 
-        <meta name="keywords" content="build, react, node, buildspace, gpt-3, ai, ml">
-        <meta name="robots" content="index, follow">
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <meta name="language" content="English">
+        <meta name="keywords" content="build, react, node, buildspace, gpt-3, ai, ml"></meta>
+        <meta name="robots" content="index, follow"></meta>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
+        <meta name="language" content="English"></meta>
 
-        <!-- Primary Meta Tags -->
+        // Primary Meta Tags
         <title>GTFOL</title>
-        <meta name="title" content="Tea time with Marcus Aurelius">
-        <meta name="description" content="Ever wonder what it would be like to sit down and chat with Marcus Aurelius? Now you can with the power of GPT-3">
+        <meta name="title" content="Tea time with Marcus Aurelius"></meta>
+        <meta name="description" content="Ever wonder what it would be like to sit down and chat with Marcus Aurelius? Now you can with the power of GPT-3"></meta>
 
-        <!-- Open Graph / Facebook -->
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="https://marcus.newagestoic.com">
-        <meta property="og:title" content="GTFOL">
-        <meta property="og:description" content="Ever wonder what it would be like to sit down and chat with Marcus Aurelius? Now you can with the power of GPT-3">
-<meta property="og:image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png">
+        // Open Graph / Facebook 
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:url" content="https://marcus.newagestoic.com"></meta>
+        <meta property="og:title" content="GTFOL"></meta>
+        <meta property="og:description" content="Ever wonder what it would be like to sit down and chat with Marcus Aurelius? Now you can with the power of GPT-3"></meta>
+        <meta property="og:image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png"></meta>
 
 
       </Head>
