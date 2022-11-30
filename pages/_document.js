ff50ec5a926e9c9c9e8e19074b19d18b0c1c3aa2
johnamcconnell <john.a.mcconnell@gmail.com>
2022-11-30 09:08:43 -0500

    fixed the meta images

--- pages/_document.js
+++ pages/_document.js
@@ -8,19 +8,19 @@ export default function Document() {
         <meta property="og:description" content="built by John 🚢 together with buildspace" key="description"/>
         <meta
           property="og:image"
-          content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png"
+          content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/Screenshot%202022-11-27%20at%209.38.10%20PM.ZOzRbyWvJUArSeOzLCDgmw.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
         <meta name="twitter:title" content="Tea time with Marcus Aurelius"></meta>
         <meta name="twitter:description" content="Ever wonder what it would be like to sit down and chat with Marcus Aurelius? Now you can with the power of GPT-3"></meta>
-        <meta itemprop="image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png"></meta>
+        <meta itemprop="image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/Screenshot%202022-11-27%20at%209.38.10%20PM.ZOzRbyWvJUArSeOzLCDgmw.png"></meta>
 
         <meta content="width=device-width, initial-scale=1" name="viewport"></meta>
 
         <link rel="icon" type="image/x-icon" href="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/marcus-aurelius-gettyimages-122316830.ZA9Ai6KCZ02q28B9AAVp0A.jpg"></link>
 
         <meta name="twitter:card" content="summary_large_image"></meta>
-        <meta name="twitter:image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png"></meta>
+        <meta name="twitter:image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/Screenshot%202022-11-27%20at%209.38.10%20PM.ZOzRbyWvJUArSeOzLCDgmw.png"></meta>
         <meta property="twitter:url" content="https://marcus.newagestoic.com"></meta>
 
         <meta name="keywords" content="build, react, node, buildspace, gpt-3, ai, ml"></meta>
@@ -36,7 +36,7 @@ export default function Document() {
         <meta property="og:url" content="https://marcus.newagestoic.com"></meta>
         <meta property="og:title" content="Ask Marcus Anything"></meta>
         <meta property="og:description" content="Ever wonder what it would be like to sit down and chat with Marcus Aurelius? Now you can with the power of GPT-3"></meta>
-        <meta property="og:image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/gtfometa.kLChOMOzM0AM47PeQ6waOw.png"></meta>
+        <meta property="og:image" content="https://documents-scus.bildr.com/rd582f3f32db04431a4b7174000513cfe/doc/Screenshot%202022-11-27%20at%209.38.10%20PM.ZOzRbyWvJUArSeOzLCDgmw.png"></meta>
 
 
       </Head>
