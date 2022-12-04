558d3be35e408a0c32f96ee3e0f2b4b3e0da611c
johnamcconnell <john.a.mcconnell@gmail.com>
2022-12-04 00:12:45 -0500

    update prompt box

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ const Home = () => {
               <div className="output">
                 <div className="output-header-container">
                   <div className="output-header prompt-box">
-                    <h3>Here is what Marcus has to say...</h3>
+                    <h3>Here is what Marcus says...</h3>
                   </div>
                 </div>
                 <div className="output-content">
